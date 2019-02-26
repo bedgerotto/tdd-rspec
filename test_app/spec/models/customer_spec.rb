b49fb63896907745cef1a28222afcda7b020c007
Bruno Gerotto <bgerotto@logfacil.com.br>
2019-02-26 08:07:36 -0300

    Traits

--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -1,10 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  it '#full_name' do
-    customer = create(:customer)
-    expect(customer.full_name).to starting_with('Sr. ')
+  it '#full_name -- sobrescrevendo atributo' do
+    customer = create(:user, name: 'Bruno') # ou create(:customer, name: 'Bruno')
+    expect(customer.full_name).to eq('Sr. Bruno')
   end
 
-  it { expect{create(:customer)}.to change{Customer.all.size}.by(1) }
+  it 'Herança' do
+    customer = create(:customer_default)
+    expect(customer.vip).to be_falsey
+  end
+
+  it '#full_name' do
+    customer = create(:customer)
+    expect(customer.full_name).to starting_with('Sr.')
+  end
+
+  it 'attributes_for' do
+    attrs = attributes_for(:customer)
+    customer = Customer.create(attrs)
+    expect(customer.full_name).to starting_with('Sr.')
+  end
+
+  it 'Atributo transitório' do
+    customer = create(:customer, upcased: true)
+
+    expect(customer.name.upcase).to eq(customer.name)
+  end
+
+  it 'Cliente masculino VIP' do
+    customer = create(:customer_male_vip)
+    expect(customer.gender).to eq('M')
+    expect(customer.vip).to eq(true)
+  end
+
+  it 'Cliente masculino default' do
+    customer = create(:customer_male_default)
+    expect(customer.gender).to eq('M')
+    expect(customer.vip).to eq(false)
+  end
+
+  it 'Cliente feminino default' do
+    customer = create(:customer_female_default)
+    expect(customer.gender).to eq('F')
+    expect(customer.vip).to eq(false)
+  end
+
+  it 'Cliente feminino VIP' do
+    customer = create(:customer_female_vip)
+    expect(customer.gender).to eq('F')
+    expect(customer.vip).to eq(true)
+  end
+
+  it { expect { create(:customer) }.to change { Customer.all.size }.by(1) }
 end
