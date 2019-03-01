0de7b2ea2bd4cd885ce90f3739f9e06f2ea9e6cb
Bruno Gerotto <bgerotto@logfacil.com.br>
2019-03-01 08:08:21 -0300

    Adicionando VCR

--- test_app/spec/httparty/httparty_spec.rb
+++ test_app/spec/httparty/httparty_spec.rb
@@ -1,7 +1,10 @@
 RSpec.describe 'HTTParty' do
-  it 'HTTParty' do
+  it 'content-type', vcr: { cassette_name: 'jsonplaceholder/posts' } do
+    # stub_request(:get, "https://jsonplaceholder.typicode.com/posts/1").
+    #   to_return(status: 200, body: '', headers: { 'content-type' => 'application/json' })
+
     response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/1')
     content_type = response.headers['content-type']
-    expect(content_type).to match(/application\/json/)
+    expect(content_type).to match(%r{application\/json})
   end
 end
