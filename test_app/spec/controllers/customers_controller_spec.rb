30133065c7c23bd07142a0fb94a80ddd8693614b
Bruno Gerotto <bgerotto@logfacil.com.br>
2019-03-14 08:21:37 -0300

    Finalizando testes de controller e iniciando tetes de views

--- test_app/spec/controllers/customers_controller_spec.rb
+++ test_app/spec/controllers/customers_controller_spec.rb
@@ -22,15 +22,42 @@ RSpec.describe CustomersController, type: :controller do
   end
 
   describe 'as logged Member' do
+    let(:customer) { create(:customer) }
     before do
       member = create(:member)
       sign_in member
     end
 
+    it { is_expected.to route(:get, '/customers').to(action: :index) }
+
+    it 'Flash Notice' do
+      customer_params = attributes_for(:customer)
+      post :create, params: { customer: customer_params }
+
+      expect(flash[:notice]).to match /successfully created/
+    end
+
+    it '#show Content-Type JSON' do
+      get :show, format: :json, params: { id: customer.id }
+
+      expect(response.content_type).to eq('application/json')
+    end
+
+    it '#create Content-Type JSON' do
+      customer_params = attributes_for(:customer)
+      post :create, format: :json, params: { customer: customer_params }
+
+      expect(response.content_type).to eq('application/json')
+    end
+
     it 'with valid params' do
       customer_params = attributes_for(:customer)
-      expect{post :create, params: { customer: customer_params }}.to change(Customer, :count).by(1)
+      expect { post :create, params: { customer: customer_params } }.to change(Customer, :count).by(1)
+    end
 
+    it 'with invalid params' do
+      customer_params = attributes_for(:customer, address: nil)
+      expect { post :create, params: { customer: customer_params } }.not_to change(Customer, :count)
     end
 
     context '#show' do
