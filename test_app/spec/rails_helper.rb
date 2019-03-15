ba7189a7dc2bf025fcb7dca295e17a2921a0b5fb
Bruno Gerotto <bgerotto@logfacil.com.br>
2019-03-15 08:08:11 -0300

    Instalando driver chrome

--- test_app/spec/rails_helper.rb
+++ test_app/spec/rails_helper.rb
@@ -35,6 +35,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Warden::Test::Helpers
 
   Shoulda::Matchers.configure do |conf|
     conf.integrate do |with|
