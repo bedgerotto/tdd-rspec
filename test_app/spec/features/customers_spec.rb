589d95eda87551ee8a27fd174948adff60c9b5a3
Bruno Gerotto <bgerotto@logfacil.com.br>
2019-03-18 08:14:11 -0300

    Fianliza testes com capybara

--- test_app/spec/features/customers_spec.rb
+++ test_app/spec/features/customers_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
+require_relative '../support/new_customer_form.rb'
 
 RSpec.feature "Customers", type: :feature, js: true do
+  let(:new_customer_form) { NewCustomerForm.new }
   before do
     member = create(:member)
     login_as(member, scope: :member)
@@ -10,6 +12,29 @@ RSpec.feature "Customers", type: :feature, js: true do
     visit(customers_path)
     page.save_screenshot('screenshot.png')
     expect(page).to have_current_path(customers_path)
+  end
+
+  it 'Ajax' do
+    visit(customers_path)
+    click_link('Add Message')
+
+    expect(page).to have_content('Yes!')
+  end
+
+  it 'find' do
+    visit(customers_path)
+    click_link('Add Message')
+    expect(find('#my-div').find('h1')).to have_content('Yes!')
+  end
+
+  it 'Creates a customer - Page Object Pattern' do
+    new_customer_form.login.visit_page.fill_in_with(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      address: Faker::Address.street_address
+    ).submit
+
+    expect(page).to have_content('Customer was successfully created')
   end
 
   it 'Creates a customer' do
