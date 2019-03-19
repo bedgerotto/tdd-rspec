70934595025bc9f933295999547d3fc09c3dd9f2
Bruno Gerotto <bgerotto@logfacil.com.br>
2019-03-19 08:04:45 -0300

    Iniciando testes de api

--- test_app/spec/requests/customers_spec.rb
+++ test_app/spec/requests/customers_spec.rb
@@ -2,9 +2,27 @@ require 'rails_helper'
 
 RSpec.describe "Customers", type: :request do
   describe "GET /customers" do
-    it "works! (now write some real specs)" do
+    it "works! 200 OK" do
       get customers_path
       expect(response).to have_http_status(200)
+    end
+
+    it "Index - JSON 200 OK" do
+      get '/customers.json'
+      expect(response).to have_http_status(200)
+      expect(response.body).to include_json(
+        [
+          id: 1
+        ]
+      )
+    end
+
+    it "Show - JSON 200 OK" do
+      get '/customers/1.json'
+      expect(response).to have_http_status(200)
+      expect(response.body).to include_json(
+        id: 1
+      )
     end
   end
 end
